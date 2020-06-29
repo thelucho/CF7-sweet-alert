06e7087fbfb7de9a57b07d8dadb7af7e32dca68b
Luciano Dichiara <luciano@studiohakuna.com>
2020-06-28 21:42:37 -0300

    Checking plugin dependency with is_plugin_active() function

--- cf7-sweet-alert.php
+++ cf7-sweet-alert.php
@@ -7,30 +7,32 @@ Author: Luciano Dichiara
 Version: 1.0
 */
 
-defined('ABSPATH') or die("Bye bye");
-define( 'URL_PATH', plugin_dir_url( __FILE__ ) );
-
-/*
- * ADD CSS files.
- */
-
-add_action('wp_enqueue_scripts', 'cf7_sweet_alert_css', 100);
-function cf7_sweet_alert_css(){
-	wp_register_style( 'cf7-sweet-alert-css', URL_PATH . 'css/cf7-sweetAlert.css' ); 
-	wp_enqueue_style( 'cf7-sweet-alert-css' );
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Get out!' );
 }
 
+// Detecting if plugin is activated
+include_once(ABSPATH.'wp-admin/includes/plugin.php');
+if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {
 
-/*
- * ADD JS files.
- */
-add_action( 'wp_enqueue_scripts', 'cf7_sweet_alert_js' );
-function cf7_sweet_alert_js() {
+	// Plugins path
+	define( 'URL_PATH', plugin_dir_url( __FILE__ ) );
 
-	wp_register_script( 'cf7_sweet_alert_core', 'https://unpkg.com/sweetalert/dist/sweetalert.min.js', array (), 1.0, true);
-	wp_enqueue_script( 'cf7_sweet_alert_core');
+	// ADD CSS files.
+	add_action('wp_enqueue_scripts', 'cf7_sweet_alert_css', 100);
+	function cf7_sweet_alert_css(){
+		wp_register_style( 'cf7-sweet-alert-css', URL_PATH . 'css/cf7-sweetAlert.css' ); 
+		wp_enqueue_style( 'cf7-sweet-alert-css' );
+	}
 
-	wp_register_script( 'cf7_sweet_alert_custom', URL_PATH . 'js/cf7-sweetAlert.js', array (), NULL, true);
-	wp_enqueue_script( 'cf7_sweet_alert_custom');
-	
+	// ADD JS files.
+	add_action( 'wp_enqueue_scripts', 'cf7_sweet_alert_js' );
+	function cf7_sweet_alert_js() {
+		wp_register_script( 'cf7_sweet_alert_core', 'https://unpkg.com/sweetalert/dist/sweetalert.min.js', array (), 1.0, true);
+		wp_enqueue_script( 'cf7_sweet_alert_core');
+
+		wp_register_script( 'cf7_sweet_alert_custom', URL_PATH . 'js/cf7-sweetAlert.js', array (), NULL, true);
+		wp_enqueue_script( 'cf7_sweet_alert_custom');
+	}
+
 }
